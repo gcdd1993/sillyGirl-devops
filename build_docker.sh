0e2fea5267fe918f05e14b7602b3ac8dbc32ffae
gcdd1993 <gcwm99@gmail.com>
2021-09-23 23:48:29 +0800

    fix

--- build_docker.sh
+++ build_docker.sh
@@ -2,8 +2,8 @@
 version=v$(date "+%Y%m%d")
 
 set -ex
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+#echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-docker build -f Dockerfile -t gcdd1993/sillyGirl:latest -t gcdd1993/sillyGirl:"$version" .
-docker push gcdd1993/sillyGirl:latest
-docker push gcdd1993/sillyGirl:"$version"
+docker build -f Dockerfile -t gcdd1993/silly-girl:latest -t gcdd1993/silly-girl:"$version" .
+docker push gcdd1993/silly-girl:latest
+docker push gcdd1993/silly-girl:"$version"
